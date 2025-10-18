2c6a69660726a2e47caff4327023af599d8f17cb
zeeshanfreelancer <zeeshankhan469224@gmail.com>
2025-10-18 06:40:34 -0700

    fix schema

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
-  description: { type: String, default: "" },
   price: { type: Number, required: true, default: 0 },
-  stock: { type: Number, default: 0 },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 }, { timestamps: true });
 
